8b2ec062e05616eb2e396de0d1ff92b5b470215e
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-09-17 08:13:25 +0530

    feat(FacialExpression): added a FacialExpression Component to analyze the facial expression

---
+++ frontend/src/components/FacialExpression.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef } from "react";
+import * as faceapi from "face-api.js";
+
+export default function FacialExpression() {
+  const videoRef = useRef();
+  const canvasRef = useRef();
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = "/models";
+      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+    };
+    const startVideo = () => {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          videoRef.current.srcObject = stream;
+        })
+        .catch((err) => console.error("Error accessing webcam: ", err));
+    };
+    const handleVideoPlay = () => {
+      setInterval(async () => {
+        const detections = await faceapi
+          .detectAllFaces(
+            videoRef.current,
+            new faceapi.TinyFaceDetectorOptions()
+          )
+          .withFaceExpressions();
+        const canvas = canvasRef.current;
+        const displaySize = {
+          width: videoRef.current.videoWidth,
+          height: videoRef.current.videoHeight,
+        };
+        faceapi.matchDimensions(canvas, displaySize);
+        const resized = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resized);
+        faceapi.draw.drawFaceExpressions(canvas, resized);
+      }, 500);
+    };
+    loadModels().then(startVideo);
+    videoRef.current &&
+      videoRef.current.addEventListener("play", handleVideoPlay);
+  }, []);
+  return (
+    <div style={{ position: "relative" }}>
+      <video
+        ref={videoRef}
+        autoPlay
+        muted
+        style={{ width: "720px", height: "560px" }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "720px",
+          height: "560px",
+        }}
+      />
+    </div>
+  );
+}
+
