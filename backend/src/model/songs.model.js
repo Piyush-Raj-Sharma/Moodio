1f78413477e411aa2de46ae0370df960f6de4f82
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-09-27 01:35:57 +0530

    created POST api & used multer middleware to access the form-data i.e. audio file

--- backend/src/model/songs.model.js
+++ backend/src/model/songs.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const songSchema = new mongoose.Schema({
   title: String,
   artist: String,
-  audioUrl: String,
+  audio: String,
 }); 
 
 const Song = mongoose.model('Song', songSchema);
--- backend/src/routes/songs.routes.js
+++ backend/src/routes/songs.routes.js
@@ -2,15 +2,17 @@ const express = require('express');
 const multer = require('multer');
 const router = express.Router();
 
-const upload = multer({storage: multer.memoryStorage()});
-// multer is a middleware that is used when we send some data like any img, video or audio, etc...
+const upload = multer({storage: multer.memoryStorage()}); //memorystorage is the server's RAM (multer will temperory store the data for sometime here)
+// multer is a middleware that is used when we need to read the data coming in the form-data format
 
-router.post('/songs', (req, res) => {
-    console.log(res.body);
-
+router.post('/songs', upload.single('audio') ,(req, res) => {
+    console.log(req.body);
+    console.log(req.file); //file will be available in req.file when we use multer middleware
+    // req.body will contain all the text fields
+    // req.file will contain the file
     res.status(201).json({
         message: 'Song added successfully',
-        song: res.body
+        song: req.body
     })
     
 })
